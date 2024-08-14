0db3e51accee6c1e51b012aabda79ebdbeeb0eed
David Perry <125750519+dperry83@users.noreply.github.com>
2024-08-14 17:23:46 -0400

    decided on Hash, with more syntax refresh needed

---
+++ template_generator.rb
@@ -0,0 +1,10 @@
+
+
+# Prompt for input for column names
+
+  column_headers = Hash.new
+
+# write a method that allows the user to enter as many column headers as they need
+
+  def user_entry
+    
