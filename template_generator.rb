fe7369e5c175c229b898ead9dd1cb81088b834e5
David Perry <125750519+dperry83@users.noreply.github.com>
2024-08-24 18:21:57 -0400

    taking input now.  next: refactor add_column method to give control to user

--- template_generator.rb
+++ template_generator.rb
@@ -1,10 +1,46 @@
 
 
+@column_headers = ["date", "name", "amount"]
 # Prompt for input for column names
 
-  column_headers = Hash.new
+  def display_headers
+    column_number = 1
+    puts "your current column headers are: "
+    @column_headers.each { |header| 
+      print "#{column_number}. #{header} \n"
+      column_number += 1
+    }
+  end
 
-# write a method that allows the user to enter as many column headers as they need
+display_headers
 
-  def user_entry
+# Get next command from user
+  #next step: refactor this method so that it continues running while the user inputs header names
+  def add_columns
+    print "would you like to add columns? (y/n): "
+    response = gets.downcase.chomp!
     
+    unless response == "n"
+      print "Please type your next column name: "
+        next_name = gets.downcase.chomp!
+        @column_headers.push(next_name)
+      puts "#{next_name.to_s} added to spreadsheet.  Would you like to add another?"
+        response = gets.downcase.chomp!
+    else
+      puts "then your template is ready to print: \n"
+      display_headers
+    end
+  end
+
+=begin  
+  case response
+    when "y"
+      print "type your column name and press enter: "
+      next_name = gets.downcase.chomp!
+      @column_headers.push(next_name)
+    else puts "then your .csv file is ready to download"
+  end
+=end
+
+add_columns
+
