b95e30b6814558e815ab778eb2f535fae8633cf1
David Perry <125750519+dperry83@users.noreply.github.com>
2024-08-31 19:04:28 -0400

    simplified and encapsulated some code here.  next step: work out how to return to controller when no more columns to add

--- template_generator.rb
+++ template_generator.rb
@@ -1,7 +1,6 @@
 
 
 @column_headers = ["date", "name", "amount"]
-# Prompt for input for column names
 
   def display_headers
     column_number = 1
@@ -11,36 +10,31 @@
       column_number += 1
     }
   end
-
-display_headers
-
-# Get next command from user
-  #next step: refactor this method so that it continues running while the user inputs header names
-  def add_columns
-    print "would you like to add columns? (y/n): "
-    response = gets.downcase.chomp!
-    
-    unless response == "n"
+  
+  def add_columns(input)
+    display_headers
+    while input == "y"
       print "Please type your next column name: "
         next_name = gets.downcase.chomp!
         @column_headers.push(next_name)
-      puts "#{next_name.to_s} added to spreadsheet.  Would you like to add another?"
-        response = gets.downcase.chomp!
-    else
-      puts "then your template is ready to print: \n"
+      puts "#{next_name.to_s} added to spreadsheet: "
       display_headers
+      print "would you like to add more columns? (y/n): " #need to work out how to pass control back to calling method
+      input = gets.downcase.chomp!
     end
   end
 
-=begin  
-  case response
-    when "y"
-      print "type your column name and press enter: "
-      next_name = gets.downcase.chomp!
-      @column_headers.push(next_name)
-    else puts "then your .csv file is ready to download"
+def controller(res) 
+  if res == "y"
+    add_columns (res)
+  elsif res == "n"
+    puts "then your spreadsheet is ready to print: "
+    display_headers
+  else
+    puts "I don't understand that input. Please try again."
   end
-=end
+end
 
-add_columns
-
+  print "Welcome!  Would you like to add columns to your spreadsheet? (y/n) "
+    response = gets.downcase.chomp!
+    controller(response) 
