87dc65085d5e1bba66b32a380add1700f716143b
dperry83 <125750519+dperry83@users.noreply.github.com>
2024-09-04 16:45:48 -0400

    next step: work with creating a .csv from input data

--- csv_practice.rb
+++ csv_practice.rb
@@ -1,13 +1,18 @@
 require "csv"
 
 films_info = []
+headers = nil
 
-CSV.foreach("working_with_csv/films.csv") do |row|
-  films_info << row
+# CSV.foreach("working_with_csv/films.csv") do |row|
+#   films_info << row
+# end
+
+CSV.foreach("working_with_csv/films.csv", headers: true, header_converters: :symbol) do |row|
+    headers ||= row.headers
+    films_info << row.to_h
+  end
+
+
+films_info[1].each do |k,v|
+  puts "#{k}: #{v}"
 end
-
-
-
-# films_info.each { |line| 
-#   print line
-# }
